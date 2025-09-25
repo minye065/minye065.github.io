8330a16c612ba0ccacc58344cc0219bc7d25ee47
minye065 <minye065@fusdk12.net>
2025-09-25 10:00:32 -0700

    basepath hopefully fixed

--- next.config.ts
+++ next.config.ts
@@ -13,8 +13,8 @@ const nextConfig: NextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  //basePath: "/docs",
-
+  basePath: "/minye065.github.io",
+  
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
