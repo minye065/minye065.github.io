c0f30b954fb18725436ca6ff0ded445d9e9a7763
minye065 <minye065@fusdk12.net>
2025-09-25 09:40:22 -0700

    remove export for sign in link

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,5 @@
 import Layout from 'minye65.github.io/docs/components/layout';
 
-export function SignInLink(){
-  return (
-    <Link 
-      href = "/sign-in"
-      >
-    </Link>
-  )
-}
-
 export default function HomePage() {
   return (
     <div>
